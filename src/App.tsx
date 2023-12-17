79983089706fa4956e9722eeabf6f5aca76619a4
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2023-12-17 13:40:28 +1100

    Added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import TodoApp from "./TodoApp"
+import TodoApp from "./TodoApp";
 
 function App() {
-
   return (
     <>
       <h1>To-do List</h1>
       <TodoApp />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/CompletedTodos.tsx
+++ src/CompletedTodos.tsx
@@ -1,28 +1,31 @@
 import { Todo } from "./types";
 
-export default function CompletedTodos({ completedTodos }: { completedTodos: Todo[] }) {
-    return (
-        <>
-            <h2>Completed</h2>
-            <CompletedTodosList completedTodos={completedTodos} />
-        </>
-    );
+export default function CompletedTodos({
+  completedTodos,
+}: {
+  completedTodos: Todo[];
+}) {
+  return (
+    <>
+      <h2>Completed</h2>
+      <CompletedTodosList completedTodos={completedTodos} />
+    </>
+  );
 }
 
 function CompletedTodosList({ completedTodos }: { completedTodos: Todo[] }) {
-    return (
-        <ul>
-            {completedTodos.map(todo => {
-                return (
-                    <li key={todo.id}>
-                        <label style={{textDecoration: 'line-through'}}>
-                            {todo.description}{' '}
-                        <input type="checkbox" disabled={true} checked={todo.completed} />
-                        </label>
-                    </li>
-
-                );
-            })}
-        </ul>
-    );
+  return (
+    <ul>
+      {completedTodos.map((todo) => {
+        return (
+          <li key={todo.id}>
+            <label style={{ textDecoration: "line-through" }}>
+              {todo.description}{" "}
+              <input type="checkbox" disabled={true} checked={todo.completed} />
+            </label>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
